98d47c656d6d0905d25d98e97a84eedeaa91492d
Михаил <ivashevt0p@gmail.com>
2023-08-13 11:45:17 +0500

    дз 9 лекция

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 class Advertisement(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        verbose_name='пользователь',
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        'изображение',
+        upload_to='advertisements/'
+    )
+
     title = models.CharField('заголовок', max_length=128)
 
     # Описание товара/информация о товаре
@@ -42,6 +59,13 @@ class Advertisement(models.Model):
             )
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     
+    @admin.display(description='Фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{}" style="max-width:80px; max-height:80px"',
+                self.image.url
+            )
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
